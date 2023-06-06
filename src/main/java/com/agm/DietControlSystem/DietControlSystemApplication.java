0222dc92a544a6b8e3260ada29ac9c52356ebd92
Antonio Molis <agm8dev@gmail.com>
2023-06-06 12:47:06 +0200

    First commit. Starting point

---
+++ src/main/java/com/agm/DietControlSystem/DietControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.agm.DietControlSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietControlSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agm/DietControlSystem/DietControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agm.DietControlSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
